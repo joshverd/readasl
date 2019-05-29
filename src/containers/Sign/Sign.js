43f758cada8ea1c5c816e04b089f97d55ab12d36
unknown <joshverd2@gmail.com>
2019-05-29 13:10:45 -0500

    S

---
+++ src/containers/Sign/Sign.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import style from './Sign.scss';
+
+class Sign extends Component {
+  contructor(props) {
+    super(props);
+  }
+  render() {
+
+  }
+}
+
+export default Sign;
